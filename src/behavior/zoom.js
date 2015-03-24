dd32772dc6d055beb87d1b27f51daee53c8404fd
ilyabo <Ilya Boyandin>
2015-03-24 14:47:39 +0100

    not changing center0 position in mousewheeled while the wheel is rotating

--- src/behavior/zoom.js
+++ src/behavior/zoom.js
@@ -325,13 +325,12 @@ d3.behavior.zoom = function() {
 
   function mousewheeled() {
     var dispatch = event.of(this, arguments);
-    center0 = center || d3.mouse(this);
     if (mousewheelTimer) clearTimeout(mousewheelTimer);
-    else translate0 = location(center0), d3_selection_interrupt.call(this), zoomstarted(dispatch);
+    else translate0 = location(center0 = center || d3.mouse(this)), d3_selection_interrupt.call(this), zoomstarted(dispatch);
     mousewheelTimer = setTimeout(function() { mousewheelTimer = null; zoomended(dispatch); }, 50);
     d3_eventPreventDefault();
     scaleTo(Math.pow(2, d3_behavior_zoomDelta() * .002) * view.k);
-    translateTo(center0, translate0);
+    if (center0) translateTo(center0, translate0);
     zoomed(dispatch);
   }
 
